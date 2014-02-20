9a86f23d441dfa1a3635fd99d84e4c15db0839fc
Anton Gerasimov <me@zyxmasta.com>
2014-02-20 13:34:14 +0600

    initial+

---
+++ index.js
@@ -0,0 +1,13 @@
+/*
+ * gulp-urlrevs
+ * https://github.com/Wu-Wu/gulp-urlrevs
+ *
+ * Copyright (c) 2014 Anton Gerasimov
+ * Licensed under the MIT license.
+ */
+
+"use strict";
+
+module.exports = function (options) {
+
+};

27e2c99d4ea06652aa04e3e628dec33788142544
Anton Gerasimov <me@zyxmasta.com>
2014-02-21 22:39:28 +0600

    added some methods stubs

--- index.js
+++ index.js
@@ -21,6 +21,53 @@ var raiseError = function (err) {
 
 var UrlRevs = function (options) {
 
+    // merge with defaults
+    this.options = _.extend({
+        abbrev      : 6,
+        branch      : 'HEAD',
+        filter      : '\\.(png|jpg|jpeg|gif)',
+        path        : 'root/i',
+        prefix      : 'root',
+        valid       : [ '^\\/' ],
+        skip        : [ '^https?:\\/\\/', '^\\/\\/', '^data:image\\/(sv|pn)g', '^%23' ],
+        implant     : true,
+        upcased     : true,
+        autocommit  : true,
+        message     : 'Wave a magic wand (by urlrevs)'
+    }, options);
+
+    // tree container
+    this.tree = {};
+
+    // regexes
+    this.reFilter = null;   // ..images
+    this.reValid  = [];     // ..allowed urls
+    this.reSkip   = [];     // ..skipped urls
+
+    this.buildTree();
+
+    this.compileRegexes();
+};
+
+// build revision's tree
+UrlRevs.prototype.buildTree = function() {
+    // body...
+};
+
+// compile some regexes..
+UrlRevs.prototype.compileRegexes = function(first_argument) {
+    var reCreateNew = function (re) {
+        return new RegExp(re, 'i');
+    };
+
+    this.reFilter = reCreateNew(this.options.filter);
+    this.reValid  = _.map(this.options.valid, reCreateNew);
+    this.reSkip   = _.map(this.options.skip, reCreateNew);
+};
+
+// process url()
+UrlRevs.prototype.processURL = function() {
+    // body...
 };
 
 module.exports.create = function (options) {
